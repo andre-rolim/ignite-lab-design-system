70b3e4ab4897c005654b088c91f59104b335fab2
unknown <andrel.rolimj@gmail.com>
2022-11-04 18:16:45 -0300

    initial comit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  return (
+    <div className="App">
+      <>
+        <h1 className="font-bold text-2xl text-gray-400">Hello World!!</h1>
+
+        <button className="bg-cyan-300 font-medium px-5 py-2 rounded text-white hover:bg-cyan-500">
+          Enviar
+        </button>
+      </>
+    </div>
+  );
+}
+
+export default App;
